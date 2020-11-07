0bf0f37b9eb3e5c08f7ac52c4a845d127888c2c0
shankkalra <shankkalra@gmail.com>
2020-11-07 20:02:20 +0530

    Adding BitWise Solutions

---
+++ src/bitwise/problemsInSession/KThBit.java
@@ -0,0 +1,31 @@
+package bitwise.problemsInSession;
+
+import math_problems.problemsInSession.GetNumber;
+
+public class KThBit {
+
+    public static void main(String[] args) {
+        int n = GetNumber.getNumber();
+        int k = GetNumber.getNumber();
+
+        findKthBitIsSet(n,k);
+        findKthBitIsSet1(n,k);
+
+    }
+
+    //Using Left Shift
+    private static void findKthBitIsSet(int n, int k) {
+        if((n & (1 << (k-1) ))!=0)
+            System.out.println("SET");
+        else
+            System.out.println("Not SET");
+    }
+
+    //Using Right Shift
+    private static void findKthBitIsSet1(int n, int k) {
+        if(((n >> (k-1)) & 1 ) == 1)
+            System.out.println("SET");
+        else
+            System.out.println("Not SET");
+    }
+}
