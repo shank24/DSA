acd1f8795a107beb3cfbe104d1d334436c62b4c2
shankkalra <shankkalra@gmail.com>
2020-11-11 21:07:58 +0530

    Adding Math Prob

---
+++ src/math_problems/problemsForPractice/IsPrimeCheck.java
@@ -0,0 +1,28 @@
+package math_problems.problemsForPractice;
+
+import math_problems.problemsInSession.GetNumber;
+
+public class IsPrimeCheck {
+    public static void main(String[] args) {
+
+        int n = GetNumber.getNumber();
+
+        System.out.println(isPrime(n));
+
+    }
+
+    private static boolean isPrime(int n) {
+        boolean res=true;
+
+        if(n==1)
+            res=false;
+
+        for (int i = 2; i<=Math.sqrt(n) ; i++) {
+            if(n%i==0) {
+                res = false;
+                break;
+            }
+        }
+        return res;
+    }
+}
