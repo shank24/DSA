841590a8f4c0fdb87774933eb2e06536cdb4ea80
shankkalra <shankkalra@gmail.com>
2020-12-05 09:24:42 +0530

    Adding Kth Bit Set

---
+++ src/bitwise/problemsForPratice/CheckKthbitSet.java
@@ -0,0 +1,22 @@
+package bitwise.problemsForPratice;
+
+import math_problems.problemsInSession.GetNumber;
+
+public class CheckKthbitSet {
+    public static void main(String[] args) {
+        int n = GetNumber.getNumber();
+        int k = GetNumber.getNumber();
+
+        System.out.println(checkKthBitIsSet(n,k));
+        System.out.println(findKthBitIsSet(n,k));
+}
+    // Using Right Shift
+    private static boolean checkKthBitIsSet(int n, int k) {
+        return ( ((n>>k) & 1)==1);
+    }
+
+    //Using Left Shift
+    private static boolean findKthBitIsSet(int n, int k) {
+        return ((n & (1 << (k) ))!=0);
+    }
+}
