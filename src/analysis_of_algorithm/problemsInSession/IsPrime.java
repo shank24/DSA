b5547dd809146340105bff05ca92c3dc2e6393cd
shankkalra <shankkalra@gmail.com>
2021-05-08 19:15:27 +0530

    Big Oh

--- src/analysis_of_algorithm/problemsInSession/IsPrime.java
+++ src/analysis_of_algorithm/problemsInSession/IsPrime.java
@@ -1,4 +1,33 @@
 package analysis_of_algorithm.problemsInSession;
 
+import math_problems.problemsInSession.GetNumber;
+
 public class IsPrime {
+
+    public static void main(String[] args) {
+
+        int n = GetNumber.getNumber();
+
+        System.out.println(FindPrime(n));
+    }
+
+    private static boolean FindPrime(int n) {
+
+        if(n==1)
+            return false;
+
+        if(n==2 || n==3)
+            return true;
+
+        if(n%2==0 || n%3==0 )
+            return false;
+
+        for (int i = 5; i*i <=n; i=i+6) {
+                if(n%i==0 || n%(i+2)==0){
+                    return false;
+            }
+        }
+        return true;
+    }
+    //Time Complexity - (Big-O(Sqrt(n)))
 }
