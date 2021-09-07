e460da6938771c924fdc0ad8523cc2ae1cb1455b
shankkalra <shankkalra@gmail.com>
2021-09-07 18:52:15 +0530

    Sol-1

---
+++ src/Hacker_Rank/Set_1_30_Days_Code/Solution_1.java
@@ -0,0 +1,10 @@
+package Hacker_Rank.Set_1_30_Days_Code;
+
+public class Solution_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+
+}
