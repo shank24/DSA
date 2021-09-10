73be0cc986911164f20e02d902249de2e73bcb8a
shankkalra <shankkalra@gmail.com>
2021-09-10 19:32:00 +0530

    Odd

--- src/Hacker_Rank/Set_1/Solution_3.java
+++ src/Hacker_Rank/Set_1/Solution_3.java
@@ -7,7 +7,25 @@ public class Solution_3 {
     public static void main(String[] args) {
 
     int num = GetNumber.getNumber();
-        System.out.println(num);
-
+        System.out.println(printWeird(num));
     }
+
+    private static int printWeird(int n) {
+
+        if(n%2!=0){
+            System.out.println("Weird");
+        }
+        else if(n%2==0 && (n>2 && n<=5)){
+            System.out.println("Not Weird");
+        }
+        else if(n%2==0 && (n>6 && n<=20)){
+            System.out.println("Weird");
+        }
+        else if(n%2==0 && n>20){
+            System.out.println("Not Weird");
+        }
+        return n;
+    }
+
+
 }
