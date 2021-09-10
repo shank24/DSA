569a7320f9941f38c66ba16f73ea51f3bf69d069
shankkalra <shankkalra@gmail.com>
2021-09-10 19:27:02 +0530

    Sol3

--- src/Hacker_Rank/Set_1/Solution_3.java
+++ src/Hacker_Rank/Set_1/Solution_3.java
@@ -1,10 +1,13 @@
 package Hacker_Rank.Set_1;
 
+import Hacker_Rank.Util.GetNumber;
+
 public class Solution_3 {
 
     public static void main(String[] args) {
 
-
+    int num = GetNumber.getNumber();
+        System.out.println(num);
 
     }
 }
---
+++ src/Hacker_Rank/Util/GetNumber.java
@@ -0,0 +1,13 @@
+package Hacker_Rank.Util;
+
+import java.util.Scanner;
+
+public class GetNumber {
+
+     public static int getNumber() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the Number : ");
+        return scanner.nextInt();
+    }
+}
