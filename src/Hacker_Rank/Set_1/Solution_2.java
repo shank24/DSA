051eda00e75fd2b3129db88c88c7face89971df8
shankkalra <shankkalra@gmail.com>
2021-09-10 18:57:16 +0530

    Numbers

---
+++ src/Hacker_Rank/Set_1/Solution_2.java
@@ -0,0 +1,22 @@
+package Hacker_Rank.Set_1;
+
+
+import java.util.Scanner;
+
+public class Solution_2 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the 3 numbers");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+    }
+}
