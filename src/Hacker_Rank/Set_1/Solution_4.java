75f4a0e6613c0eb9d44a25f17b43533e939cff02
shankkalra <shankkalra@gmail.com>
2021-09-16 14:15:21 +0530

    Java.util.formatter

---
+++ src/Hacker_Rank/Set_1/Solution_4.java
@@ -0,0 +1,17 @@
+package Hacker_Rank.Set_1;
+
+import java.util.Scanner;
+
+public class Solution_4 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        String s = scan.next();
+        Double d = scan.nextDouble();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
---
+++ src/Hacker_Rank/Set_1/Solution_5.java
@@ -0,0 +1,28 @@
+package Hacker_Rank.Set_1;
+
+import java.util.Scanner;
+
+public class Solution_5 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String s1=sc.next();
+            int x=sc.nextInt();
+            //Complete this line
+            System.out.printf("'%-15s'%03d%n", s1,x);
+
+            /*
+            %s - string
+            %d - decimal
+            %n - New Line
+            %f - floating points
+             */
+
+        }
+        System.out.println("================================");
+        System.out.printf("baeldung%nline%nterminator%n");
+        System.out.printf("'%S' %n", "baeldung");
+    }
+
+}
