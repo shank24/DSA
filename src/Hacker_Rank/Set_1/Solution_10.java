fb2bed2466e47a7df8e0ad5bfcde5275db4faaa2
shankkalra <shankkalra@gmail.com>
2021-09-18 18:09:44 +0530

    Excecption Print

---
+++ src/Hacker_Rank/Set_1/Solution_10.java
@@ -0,0 +1,33 @@
+package Hacker_Rank.Set_1;
+
+
+public class Solution_10 {
+
+    static int B;
+    static int H;
+    static {
+            B=1;
+            H=-3;
+    }
+
+    public static void main(String[] args) {
+
+        boolean flag = true;
+
+        try {
+            if(B <= 0 || H <= 0) {
+                flag = false;
+                throw new Exception("Breadth and height must be positive");
+            }
+            else{
+                System.out.println(B*H);
+            }
+        }
+        catch (Exception e){
+            System.out.println(e.toString());
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
