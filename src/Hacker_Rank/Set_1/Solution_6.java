eccc6ef1ddbb159fb4b52bb930a32bde81b81050
shankkalra <shankkalra@gmail.com>
2021-09-18 12:26:57 +0530

    Sol_6

---
+++ src/Hacker_Rank/Set_1/Solution_6.java
@@ -0,0 +1,16 @@
+package Hacker_Rank.Set_1;
+
+import java.util.Scanner;
+
+public class Solution_6 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i=1;i<=10;i++){
+            System.out.printf("%d X %d= %d%n",n,i,n*i);
+        }
+    }
+
+}
