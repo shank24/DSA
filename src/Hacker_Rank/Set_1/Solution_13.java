e5d87e8ebb174dd9254867f045ae0134c240c0fa
shankkalra <shankkalra@gmail.com>
2021-09-22 23:05:02 +0530

    Sol_13_Locale

---
+++ src/Hacker_Rank/Set_1/Solution_13.java
@@ -0,0 +1,39 @@
+package Hacker_Rank.Set_1;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Solution_13 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+
+        Locale inrLocale = new Locale("en","IN");
+
+        NumberFormat us = getNumberFormat(Locale.US);
+        NumberFormat china = getNumberFormat(Locale.CHINA);
+        NumberFormat france = getNumberFormat(Locale.FRANCE);
+        NumberFormat india = NumberFormat.getCurrencyInstance(inrLocale);
+
+
+        printResult(payment, us, "US: ");
+        printResult(payment, india, "India: ");
+        printResult(payment, china, "China: ");
+        printResult(payment, france, "France: ");
+    }
+
+    private static void printResult(double payment, NumberFormat numberFormat, String s) {
+        System.out.println(s + numberFormat.format(payment));
+    }
+
+    private static NumberFormat getNumberFormat(Locale type) {
+        return NumberFormat.getCurrencyInstance(type);
+    }
+
+}
