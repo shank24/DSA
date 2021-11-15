5c5f2c2bb42e9a681e564eccfeea60f2c8328cfb
shankkalra <shankkalra@gmail.com>
2021-11-15 16:27:13 +0530

    Get Set Bit Code

--- src/GFG/Bitwise/problemsForPratice/GetSetBits.java
+++ src/GFG/Bitwise/problemsForPratice/GetSetBits.java
@@ -7,7 +7,9 @@ public class GetSetBits {
 
         int n = GetNumber.getNumber();
 
-        System.out.println(getFirstSetBits(n));
+        //System.out.println(getFirstSetBits(n));
+
+        System.out.println(getFirstSetBitsOtherWay(n));
     }
 
     private static int getFirstSetBits(int n) {
@@ -18,4 +20,24 @@ public class GetSetBits {
         }
         return (int)(Math.log(((n^(n-1))+1))/Math.log(2));
     }
+
+    private static int getFirstSetBitsOtherWay(int n) {
+
+        int res=0,count=1;
+
+        if(n==0){
+            return 0;
+        }
+
+        while (n!=0){
+            if((n & 1) ==1){
+                res=count;
+                break;
+            }
+            count++;
+            n=n/2;
+        }
+        return count;
+    }
+
 }
