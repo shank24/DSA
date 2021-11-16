52ad38aec4bedc93d5e7562c16c5f57cd95563c7
shankkalra <shankkalra@gmail.com>
2021-11-16 23:17:17 +0530

    Is Parse

---
+++ src/GFG/Bitwise/problemsForPratice/IsParse.java
@@ -0,0 +1,19 @@
+package GFG.Bitwise.problemsForPratice;
+
+import GFG.Math_problems.problemsInSession.GetNumber;
+
+public class IsParse {
+    public static void main(String[] args) {
+
+
+        int a = GetNumber.getNumber();
+
+
+        System.out.println(isSparse(a));
+    }
+
+    public static boolean isSparse(int n)
+    {
+            return ((n&(n>>1))==0);
+    }
+}
