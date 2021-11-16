03ea95f642f8759552b3787e29d3c7510d02725a
shankkalra <shankkalra@gmail.com>
2021-11-16 17:41:03 +0530

    Bit Diff Code

---
+++ src/GFG/Bitwise/problemsForPratice/BitDifference.java
@@ -0,0 +1,33 @@
+package GFG.Bitwise.problemsForPratice;
+
+import GFG.Math_problems.problemsInSession.GetNumber;
+
+public class BitDifference {
+    public static void main(String[] args) {
+
+        System.out.println("Enter the First Number");
+        int a = GetNumber.getNumber();
+
+        System.out.println("Enter the Second Number");
+        int b = GetNumber.getNumber();
+
+        System.out.println(countBitsFlip(a,b));
+    }
+
+    public static int countBitsFlip(int a, int b){
+
+        int count=0;
+        if(a==b)
+            return 0;
+
+        int x = (a^b);
+
+        while (x>0){
+            x = (x & (x-1));
+            count++;
+        }
+
+        return count;
+
+    }
+}
