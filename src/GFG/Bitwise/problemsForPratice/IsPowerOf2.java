f67f4708aea97ce9c619266f5f130b24cb345326
shankkalra <shankkalra@gmail.com>
2021-11-17 21:32:35 +0530

    Is Power Of 2

--- src/GFG/Bitwise/problemsForPratice/IsPowerOf2.java
+++ src/GFG/Bitwise/problemsForPratice/IsPowerOf2.java
@@ -1,8 +1,45 @@
 package GFG.Bitwise.problemsForPratice;
 
+import GFG.Math_problems.problemsInSession.GetNumber;
+
 public class IsPowerOf2 {
 
     public static void main(String[] args) {
 
+        int a = GetNumber.getNumber();
+        System.out.println(isPowerofTwoSimpleWay(a));
+
+    }
+
+    /*public static boolean isPowerofTwo(long n){
+
+        if(n==0)
+            return false;
+
+        return (!(n & (n - 1)));
+
+    }*/
+
+    /*
+    A number which is power of 2 will only have 1
+    set bit except 0 which is the base case.
+    So by changing the rightmost set bit to unset bit
+    n&(n-1)we see if it is equal to 0.
+    If yes then return true else false.
+     */
+
+
+    public static boolean isPowerofTwoSimpleWay(long n){
+        // Your code here
+        if(n==0){
+            return false;
+        }
+        while(n!=1){
+            if(n%2!=0){
+                return false;
+            }
+            n=n/2;
+        }
+        return true;
     }
 }
