df52f9e82ec19bec8a822883967f8e3e4f1ec6b0
shankkalra <shankkalra@gmail.com>
2021-11-30 23:37:52 +0530

    Log Base 3

--- src/GFG/Recursion/problemsInSession/Recursion_Riddles.java
+++ src/GFG/Recursion/problemsInSession/Recursion_Riddles.java
@@ -8,8 +8,9 @@ public class Recursion_Riddles {
 
         int n = GetNumber.getNumber();
         //fun(n);
-        fun1(n);
+        //fun1(n);
         //System.out.println(fun2(n));
+        System.out.println(fun3(n));
     }
 
     static void fun(int n){
@@ -31,11 +32,20 @@ public class Recursion_Riddles {
     }
 
 
+    //Log(n - base 2)
     static int fun2(int n){
         if(n==1)
             return 0;
         else
             return 1 + fun2(n/2);
+    }
+
+    //Log (n - base 3)
+    static int fun3(int n){
+        if(n<3)
+            return 0;
+        else
+            return 1 + fun3(n/3);
     }
 
 
