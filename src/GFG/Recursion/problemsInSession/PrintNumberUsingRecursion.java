59586b7b3360e43f6e10c466588ad475ca5b5ebf
shankkalra <shankkalra@gmail.com>
2021-12-01 23:30:09 +0530

    Tail Recursion

--- src/GFG/Recursion/problemsInSession/PrintNumberUsingRecursion.java
+++ src/GFG/Recursion/problemsInSession/PrintNumberUsingRecursion.java
@@ -8,10 +8,14 @@ public class PrintNumberUsingRecursion {
 
         int n = GetNumber.getNumber();
 
-        oneToN(n);
-        nToOne(n);
+        //oneToN(n);
+        //nToOne(n);
+        oneToNTailRecursive(n,1);
     }
 
+    // Non Tail Recursive Function
+    // Here it is storing the state
+    // and then printing
     static void oneToN(int n){
         if (n==0)
             return;
@@ -20,6 +24,19 @@ public class PrintNumberUsingRecursion {
         System.out.println(n);
     }
 
+    static void oneToNTailRecursive(int n, int k){
+
+        if (n==0)
+            return;
+
+        System.out.println(k);
+        oneToNTailRecursive(n-1,k+1);
+    }
+
+
+    // Tail Recursive Function
+    // Here it is not storing any call
+    // after child call's
     static void nToOne(int n){
         if (n==0)
             return;
