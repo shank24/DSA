786633b73a86d13bbcdbf29cbceb9d49de007d6b
shankkalra <shankkalra@gmail.com>
2021-12-04 15:45:48 +0530

    Fibbo

--- src/GFG/Recursion/problemsInSession/Fibbo_Rec.java
+++ src/GFG/Recursion/problemsInSession/Fibbo_Rec.java
@@ -1,7 +1,23 @@
 package GFG.Recursion.problemsInSession;
 
+import GFG.Math_problems.problemsInSession.GetNumber;
+
 public class Fibbo_Rec {
     public static void main(String[] args) {
 
+        int n = GetNumber.getNumber();
+
+        System.out.println(fibbo(n));
     }
+
+    private static int fibbo(int n) {
+
+        if(n<=1){
+            return n;
+        }
+
+        return fibbo(n-1) + fibbo(n-2);
+    }
+
+
 }
