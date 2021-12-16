838f4398811fe1e0fd1eca087236767ffec38e85
shankkalra <shankkalra@gmail.com>
2021-12-16 23:18:32 +0530

    Digital Roots

---
+++ src/GFG/Recursion/problemsForPractice/Digital_Roots.java
@@ -0,0 +1,44 @@
+package GFG.Recursion.problemsForPractice;
+
+import GFG.Math_problems.problemsInSession.GetNumber;
+
+public class Digital_Roots {
+    public static void main(String[] args) {
+        int n = GetNumber.getNumber();
+
+        System.out.println(getDigitalRoots(n));
+        //System.out.println(getDigitalRootsRec(n));
+
+    }
+
+    private static int getDigitalRootsRec(int n) {
+        if(n == 0) return 0;
+        if(n % 9  == 0) return 9;
+        return (n % 9);
+    }
+
+    private static int getDigitalRoots(int n) {
+
+        int sum = 0;
+
+        // Loop to do sum while
+        // sum is not less than
+        // or equal to 9
+        while (n > 0 || sum > 9)
+        {
+            if (n == 0) {
+                n = sum;
+                sum= 0;
+            }
+
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+
+
+    }
+
+
+
+}
