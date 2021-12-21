076e2d92777a84dc80e1ef55842a93a2091bf66d
shankkalra <shankkalra@gmail.com>
2021-12-21 11:56:49 +0530

    Subsets Sum Iterative Sol

--- src/GFG/Recursion/problemsInSession/Generate_Sum_Subsets.java
+++ src/GFG/Recursion/problemsInSession/Generate_Sum_Subsets.java
@@ -4,11 +4,11 @@ public class Generate_Sum_Subsets {
     public static void main(String[] args) {
 
         int n=3;
-        int arr[] = {2,1,3};
-        int sum =4;
+        int arr[] = {10,15,20};
+        int sum =25;
 
         System.out.println(countSubsets(arr, n, sum));
-
+        System.out.println(countSubsetsIterativeWay(arr, sum));
     }
 
     static int countSubsets(int arr[], int n, int sum)
@@ -16,5 +16,20 @@ public class Generate_Sum_Subsets {
         if(n==0)
             return sum==0? 1 : 0;
         return countSubsets(arr, n-1, sum) + countSubsets(arr, n-1, sum - arr[n-1]);
+    }
+
+    static int countSubsetsIterativeWay(int arr[], int sum){
+
+        int count=0;
+
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = i; j <arr.length ; j++) {
+                if(arr[i] + arr[j] == sum){
+                    count++;
+                }
+            }
+        }
+
+        return count;
     }
 }
