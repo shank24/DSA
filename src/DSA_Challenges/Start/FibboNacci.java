8f67932a772966f3b04d65b9bb944c9b23351cf0
shankkalra <shankkalra@gmail.com>
2022-04-08 17:30:51 +0530

    Leet Code Basic

---
+++ src/DSA_Challenges/Start/FibboNacci.java
@@ -0,0 +1,10 @@
+package DSA_Challenges.Start;
+
+import GFG.util.GetNumber;
+
+public class FibboNacci {
+    public static void main(String[] args) {
+
+        int range = GetNumber.getNumber();
+    }
+}
