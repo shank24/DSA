13f7359065079e7fce94cc0eaa3c527e41ace9b8
shankkalra <shankkalra@gmail.com>
2022-04-08 17:37:56 +0530

    Fibbo

--- src/DSA_Challenges/Start/FibboNacci.java
+++ src/DSA_Challenges/Start/FibboNacci.java
@@ -6,5 +6,12 @@ public class FibboNacci {
     public static void main(String[] args) {
 
         int range = GetNumber.getNumber();
+        System.out.println(fibbo(range));
+    }
+
+    public static int fibbo(int range) {
+        if(range<=1)
+            return range;
+        return fibbo(range-1)+fibbo(range-2);
     }
 }
