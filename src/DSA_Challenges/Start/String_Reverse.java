df33bbfc637835f665c224d90df7538bb8c2c385
SHANKY KALRA <shankykalra@SHANKYs-MacBook-Pro.local>
2023-12-27 21:03:18 -0400

    Changes

--- src/DSA_Challenges/Start/String_Reverse.java
+++ src/DSA_Challenges/Start/String_Reverse.java
@@ -9,8 +9,6 @@ public class String_Reverse {
     }
 
     public static void reverse(String str) {
-        for (int i = str.length()-1; i >0 ; i--) {
-            System.out.print(str.charAt(i));
-        }
+        for (int i = str.length()-1; i >=0 ; i--) System.out.print(str.charAt(i));
     }
 }
